Fix DER computation, update configs, Add new options to conformer spk buff encoder

nemo/collections/asr/metrics/spyder.py
@@ -74,8 +74,8 @@ class MeetevalDER(Metric):
             starts = torch.where(diff == 1)[0]
             ends = torch.where(diff == -1)[0]
             assert len(starts) == len(ends)
-            for j in range(len(starts)):
-                segments.append(RTTMLine(type='SPEAKER', filename=utt_id, channel=0, begin_time=offset + starts[j].item()*self.embed_duration, duration=(ends[j]-starts[j]).item()*self.embed_duration, orthography='<NA>', speaker_type='<NA>', speaker_id=i, confidence='<NA>', signal_look_ahead_time='<NA>'))
+            for i in range(len(starts)):
+                segments.append(RTTMLine(type='SPEAKER', filename=utt_id, channel=0, begin_time=offset + starts[i].item()*self.embed_duration, duration=(ends[i]-starts[i]).item()*self.embed_duration, orthography='<NA>', speaker_type='<NA>', speaker_id=i, confidence='<NA>', signal_look_ahead_time='<NA>'))
         return segments
 
     def update(
@@ -101,7 +101,7 @@ class MeetevalDER(Metric):
                 pred = pred[:, present_pred_speakers]
                 target = target[:, present_target_speakers]
 
-                utt_id = f'{utt_id}_{str(offset.item()).replace(".", "_")}'
+                utt_id = f'{utt_id}_{offset.item()}'
                 pred_segments = self._tensor_to_segments(pred, utt_id, 0)
                 
                 if utt_id not in self.per_utt_data:
@@ -142,13 +142,8 @@ class MeetevalDER(Metric):
                 spk_time = sum(l.duration for l in all_targets_rttm.lines)
                 res_der = {utt_id: Namespace(scored_speaker_time=Decimal(spk_time), missed_speaker_time=Decimal(spk_time), falarm_speaker_time=Decimal(0), speaker_error_time=Decimal(0))}
             else:
-                try:
-                    with warnings.catch_warnings(action="ignore"):
-                        res_der = meeteval.der.dscore(reference=all_targets_rttm, hypothesis=all_preds_rttm, collar=collar)
-                except Exception as e:
-                    print(f"Error scoring {utt_id}: {e}")
-                    spk_time = sum(l.duration for l in all_targets_rttm.lines)
-                    res_der = {utt_id: Namespace(scored_speaker_time=Decimal(spk_time), missed_speaker_time=Decimal(spk_time), falarm_speaker_time=Decimal(0), speaker_error_time=Decimal(0))}
+                with warnings.catch_warnings(action="ignore"):
+                    res_der = meeteval.der.dscore(reference=all_targets_rttm, hypothesis=all_preds_rttm, collar=collar)
             results.append(res_der)
 
         results = self._process_metric_res(results)
@@ -162,7 +157,3 @@ class MeetevalDER(Metric):
         res_all_ranks = self._reduce_res(res_all_ranks)
         res_all_ranks['der'] = (res_all_ranks['speaker_error_time'] + res_all_ranks['missed_speaker_time'] + res_all_ranks['falarm_speaker_time']) / res_all_ranks['scored_speaker_time']
         return res_all_ranks
-
-    def reset(self):
-        super().reset()
-        self.per_utt_data.clear()

nemo/collections/asr/models/eend_spk_buffer_diar_model.py
@@ -929,8 +929,9 @@ class EENDSpkBuffEncLabelModel(ModelPT, ExportableEncDecModel, SpkDiarizationMix
                 pred_num = 0
                 if os.path.exists(f'{self.trainer.log_dir}'):
                     pred_num = len(list(filter(lambda x: x.startswith('pred_matrices'), os.listdir(f'{self.trainer.log_dir}')))) + 1
-                    
+
                 save_path = f'{self.trainer.log_dir}/pred_matrices_{pred_num}/{self.current_epoch}_{self.trainer.global_step}/{uniq_id}_preds.pt'
+                print(f'Saving predictions to {save_path}')
                 os.makedirs(os.path.dirname(save_path), exist_ok=True)
                 torch.save(preds[i][:target_lens[i]].detach().cpu(), save_path)
                 torch.save(targets_pil[i][:target_lens[i]].detach().cpu(), save_path.replace('_preds.pt', '_targets.pt'))
@@ -1132,14 +1133,14 @@ class EENDSpkBuffEncLabelModel(ModelPT, ExportableEncDecModel, SpkDiarizationMix
 
             output_dict['log']['val_metrics/der'] = der_output['der']
             output_dict['log']['val_metrics/der_scored_speaker_time'] = der_output['scored_speaker_time']
-            output_dict['log']['val_metrics/der_missed_speaker_time'] = der_output['missed_speaker_time']
-            output_dict['log']['val_metrics/der_falarm_speaker_time'] = der_output['falarm_speaker_time']
-            output_dict['log']['val_metrics/der_speaker_error_time'] = der_output['speaker_error_time']
+            output_dict['log']['val_metrics/der_missed_speaker_time'] = der_output['missed_speaker_time'] / der_output['scored_speaker_time']
+            output_dict['log']['val_metrics/der_falarm_speaker_time'] = der_output['falarm_speaker_time'] / der_output['scored_speaker_time']
+            output_dict['log']['val_metrics/der_speaker_error_time'] = der_output['speaker_error_time'] / der_output['scored_speaker_time']
             output_dict['log']['val_metrics/der_collar_0.25'] = der_output_collar['der']
             output_dict['log']['val_metrics/der_collar_0.25_scored_speaker_time'] = der_output_collar['scored_speaker_time']
-            output_dict['log']['val_metrics/der_collar_0.25_missed_speaker_time'] = der_output_collar['missed_speaker_time']
-            output_dict['log']['val_metrics/der_collar_0.25_falarm_speaker_time'] = der_output_collar['falarm_speaker_time']
-            output_dict['log']['val_metrics/der_collar_0.25_speaker_error_time'] = der_output_collar['speaker_error_time']
+            output_dict['log']['val_metrics/der_collar_0.25_missed_speaker_time'] = der_output_collar['missed_speaker_time'] / der_output_collar['scored_speaker_time']
+            output_dict['log']['val_metrics/der_collar_0.25_falarm_speaker_time'] = der_output_collar['falarm_speaker_time'] / der_output_collar['scored_speaker_time']
+            output_dict['log']['val_metrics/der_collar_0.25_speaker_error_time'] = der_output_collar['speaker_error_time'] / der_output_collar['scored_speaker_time']
 
             if output_dict is not None and 'log' in output_dict:
                 self.log_dict(output_dict.pop('log'), on_epoch=True, sync_dist=sync_metrics)

nemo/collections/asr/modules/conformer_encoder_spk_buffer.py
@@ -220,6 +220,7 @@ class ConformerEncoderSpkBuff(ConformerEncoder):
         sync_max_audio_length: bool = True,
         prepend_global_tokens: bool = False, # If True, add trainable tokens to the beginning of the sequence
         use_ce_spk_buffer: bool = False, # If True, use the CE speaker buffer as well at the end of the ConformerLayer.
+        use_ce_spk_buffer_bias: bool = True, # If True, use bias in the CE speaker buffer.
         share_spk_buffer: bool = False, # If True, share the speaker buffer between the ConformerLayer and the CE speaker buffer.
         spk_buffer_size: int = 128,
     ):
@@ -271,6 +272,7 @@ class ConformerEncoderSpkBuff(ConformerEncoder):
         self.use_ce_spk_buffer = use_ce_spk_buffer
         self.spk_buffer_size = spk_buffer_size
         self.share_spk_buffer = share_spk_buffer
+        self.use_ce_spk_buffer_bias = use_ce_spk_buffer_bias
 
         if self.prepend_global_tokens:
             assert self.global_tokens_spacing == 1, "global_tokens_spacing must be 1 when prepend_global_tokens is True"
@@ -292,6 +294,7 @@ class ConformerEncoderSpkBuff(ConformerEncoder):
                     dropout_rate=self.dropout_att,
                     use_pytorch_sdpa=self.use_pytorch_sdpa,
                     use_pytorch_sdpa_backends=self.use_pytorch_sdpa_backends,
+                    use_bias=self.use_ce_spk_buffer_bias, # The layer is followed by a LayerNorm, so no bias is needed (technically).
                 )
                 for _ in range(self.n_layers)
             ])
@@ -299,7 +302,7 @@ class ConformerEncoderSpkBuff(ConformerEncoder):
                 nn.LayerNorm(self.d_model)
                 for _ in range(self.n_layers)
             ])
-        print("spk_buffer", self.spk_buffer)
+        # print("spk_buffer", self.spk_buffer)
 
     @typecheck()
     def forward(