Add audio masking, new video transform settings, fixes

nemo/collections/asr/data/av_data_utils/transforms.py
@@ -9,6 +9,7 @@ import torchvision
 import random
 import numpy as np
 from python_speech_features import logfbank
+import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.transforms import functional as TF
 from torchvision.transforms.functional import InterpolationMode
@@ -234,19 +235,30 @@ def brightness_frame(frame, brightness_factor: float):
 
 
 class VideoTransform:
-    def __init__(self, subset):
+    def __init__(self, subset, perform_time_mask=True):
         if subset == "train":
+            if not perform_time_mask:
+                print("Skipping time masking on trainset.")
+
             self.video_pipeline = torch.nn.Sequential(
                 FunctionalModule(lambda x: x / 255.0),
                 torchvision.transforms.RandomCrop(88),
+
+                # ORIG
+                # torchvision.transforms.RandomRotation(10),
+                # torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2),
+                # # torchvision.transforms.Grayscale(),
+                # AdaptiveTimeMask(10, 25),
+
+                # NEW augment
                 # torchvision.transforms.RandomRotation(10),
                 # torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2),
                 torchvision.transforms.GaussianBlur((5,5), (0.01, 0.5)),
                 TemporalRandomWalk1D(
                     frame_transform=rotate_frame,
-                    p=0.9,
-                    min_val=-12.0,
-                    max_val=12.0,
+                    p=0.5,
+                    min_val=-10.0,
+                    max_val=10.0,
                     alpha=0.2,
                     sigma=2,
                     mu=0.0,
@@ -254,7 +266,7 @@ class VideoTransform:
                 ),
                 TemporalRandomWalk1D(
                     frame_transform=brightness_frame,
-                    p=0.9,
+                    p=0.5,
                     min_val=-1,
                     max_val=1.0,
                     alpha=0.2,
@@ -263,7 +275,9 @@ class VideoTransform:
                     smooth_window=3,
                 ),
                 # torchvision.transforms.Grayscale(),
-                # AdaptiveTimeMask(50, 25),
+                # AdaptiveTimeMask(25, 3), # Recordings are shorter, it's pre-training afterall.
+                AdaptiveTimeMask(10, 40) if perform_time_mask else nn.Identity(), # ORIG params, 50 - 2x less masks than 25, 10 means max mask span width.
+
                 torchvision.transforms.Normalize(0.421, 0.165),
             )
         elif subset == "val" or subset == "test":

nemo/collections/asr/data/av_to_text_and_stno_lhotse.py
@@ -43,6 +43,7 @@ from nemo.utils.get_rank import is_global_rank_zero
 from lhotse import load_manifest, CutSet, MonoCut, fastcopy
 from lhotse.cut import MixedCut
 from nemo.collections.asr.parts.preprocessing.segment import AudioSegment
+from nemo.collections.asr.data.av_data_utils.transforms import AdaptiveTimeMask
 
 __all__ = [
     'LhotseAVToBPEAndSTNODataset',
@@ -431,6 +432,10 @@ class LhotseAVToBPEAndSTNODataset(torch.utils.data.Dataset):
         self.max_random_audio_mask_ratio = max_random_audio_mask_ratio
         self.return_all_spks = return_all_spks
 
+        self.audio_masking = None
+        if not self.val:
+            self.audio_masking = AdaptiveTimeMask(6000, 32000)
+
         # Disable audio masking during validation/validation-like usage
         if self.val:
             self.randomly_mask_audio_signal = False
@@ -454,6 +459,9 @@ class LhotseAVToBPEAndSTNODataset(torch.utils.data.Dataset):
         if not self.randomly_mask_audio_signal or self.max_random_audio_mask_span_seconds <= 0.0:
             return None
         if audio_len_samples <= 0:
+            return None
+
+        if random.random() < 0.5:
             return None
 
         audio_len_seconds = audio_len_samples / float(self.sample_rate) if self.sample_rate > 0 else 0.0
@@ -636,9 +644,12 @@ class LhotseAVToBPEAndSTNODataset(torch.utils.data.Dataset):
 
         if self.return_audio:
             # Optionally apply a random contiguous zero-mask to the audio signal (refactored)
-            mask_range = self._get_random_mask_range(int(audio_data.shape[0]))
-            if mask_range is not None:
-                audio_data = self._apply_mask_to_audio(audio_data, mask_range)
+            # mask_range = self._get_random_mask_range(int(audio_data.shape[0]))
+            # if mask_range is not None:
+            #     audio_data = self._apply_mask_to_audio(audio_data, mask_range)
+
+            if self.randomly_mask_audio_signal and not self.val and self.audio_masking is not None:
+                audio_data = self.audio_masking(audio_data)
 
             downsampled_freq = self.sample_rate / self.audio_downsampling_factor
             downsampled_fl_length = audio_data_len if audio_data_len % self.audio_downsampling_factor == 0 else audio_data_len + (self.audio_downsampling_factor - (audio_data_len % self.audio_downsampling_factor))
@@ -772,7 +783,7 @@ class LhotseAVToBPEAndSTNODataset(torch.utils.data.Dataset):
                 zero_frame_idxes = np.array([], dtype=np.int64)  # Not tracking zero frames for all speakers
             else:
                 if target_spk not in cut.custom[self.video_key]:
-                    raise ValueError(f"Speaker {target_spk} not found in video paths.")
+                    raise ValueError(f"Speaker {target_spk} not found in video paths {cut.custom[self.video_key]}.")
                 
                 if self.return_all_spks:
                     all_spk_video_frames = [

nemo/collections/asr/modules/conformer_encoder_stno_av.py
@@ -218,7 +218,7 @@ class NonLinCrossFadeFusion(nn.Module):
             nn.Linear(d_model, d_model * 2),
             nn.LayerNorm(d_model * 2),
             nn.SiLU(),
-            nn.Dropout(0.1),
+            nn.Dropout(0.1), # changed from 0.3
             nn.Linear(d_model * 2, d_model)
         )
         
@@ -344,7 +344,7 @@ class VisualConditioningModule(nn.Module):
                 xscale=True,
                 dropout_rate_emb=0.1,
             )
-            self.out_dropout = nn.Dropout(0.1)
+            self.out_dropout = nn.Dropout(0.2)
 
             device = next(self.parameters()).device
             dtype = next(self.parameters()).dtype